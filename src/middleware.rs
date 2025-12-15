caaf59f5b8a01d4cda43f257de0a6d1e4d0e3108
Amelia Rossi <w.ardecan@gmail.com>
2025-12-15 07:20:56 +0100

    add removetrailingslash middleware

--- src/middleware.rs
+++ src/middleware.rs
@@ -1,4 +1,4 @@
-use std::{rc::Rc};
+use std::rc::Rc;
 
 use crate::{Context, Handler, error::Result};
 
@@ -13,10 +13,27 @@ impl Middleware for Logger {
         Rc::new(move |mut c: Context| -> Result<Context> {
             c = handler(c)?;
             println!(
-                "{} {} HTTP/1.1\n{}",
+                "{} {} HTTP/1.1\t{}",
                 c.request.method, c.request.resource, c.response.status
             );
             Ok(c)
+        })
+    }
+}
+
+pub struct RemoveTrailingSlash {}
+
+impl Middleware for RemoveTrailingSlash {
+    fn apply(&mut self, handler: Handler) -> Handler {
+        Rc::new(move |mut c: Context| -> Result<Context> {
+            c.request.resource.path = c
+                .request
+                .resource
+                .path
+                .trim_end_matches('/')
+                .to_owned()
+                .into();
+            handler(c)
         })
     }
 }
